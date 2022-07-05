5cff0543daee468d5291b26fdf9a2929841dd8e2
Nicolas Gonzalez <nicolasgonzalez180@gmail.com>
2022-07-05 22:29:27 +0200

    Update worker, specs

--- app/workers/csv_export_worker.rb
+++ app/workers/csv_export_worker.rb
@@ -4,14 +4,24 @@ class CsvExportWorker
   include Sidekiq::Worker
   sidekiq_options :queue => :default, :retry => true, :backtrace => true
 
-  def perform csv_export_id
+  def perform(csv_export_id)
     csv_export = CsvExport.find csv_export_id
-    col_sep = [:csv].include?(csv_export.format_name.to_sym) ? CSV::DEFAULT_OPTIONS.fetch(:col_sep) : "\t"
-    file = Tempfile.new
-    file.write csv_export.user.shifts.to_csv(col_sep: col_sep)
-    file.rewind
-    csv_export.file = file
+    csv_export.file = generate_file(csv_export)
     csv_export.file_name = "%s export on %s" % [csv_export.format_name.upcase, DateTime.now.strftime("%Y-%m-%d %H:%M")]
     csv_export.save!
+  end
+
+  private
+  def generate_file(csv_export)
+    file = Tempfile.new
+    file.write(csv_export.user.shifts.to_csv(col_sep: col_sep(csv_export.format_name)))
+    file.rewind
+    file
+  end
+  def col_sep(format_name)
+    case format_name.to_sym
+    when :csv then CSV::DEFAULT_OPTIONS.fetch(:col_sep)
+    when :xls then "\t"
+    end
   end
 end
--- spec/workers/csv_export_worker_spec.rb
+++ spec/workers/csv_export_worker_spec.rb
@@ -1,43 +1,47 @@
 require 'rails_helper'
 require 'sidekiq/testing'
-Sidekiq::Testing.fake!
 
 RSpec.describe CsvExportWorker, type: :worker do
   let!(:user) { FactoryBot.create(:user_with_shifts) }
   let(:csv_export) { FactoryBot.create(:csv_export) }
   describe 'testing worker' do
-    it 'jobs are enqueued in the default queue' do
-      described_class.perform_async csv_export.id
-      assert_equal :default, described_class.queue
-    end
-    it 'goes into the jobs array for testing environment' do
-      expect do
+    context 'sidekiq testing' do
+      setup do
+        Sidekiq::Testing.fake!
+      end
+      it 'jobs are enqueued in the default queue' do
         described_class.perform_async csv_export.id
-      end.to change(described_class.jobs, :size).by(1)
-    end
-    context 'csv export has no file_uid' do
-      setup do
-        csv_export.file_uid = nil
+        assert_equal :default, described_class.queue
       end
-      context 'a worker starts' do
+      it 'goes into the jobs array for testing environment' do
+        expect do
+          described_class.perform_async csv_export.id
+        end.to change(described_class.jobs, :size).by(1)
+      end
+      context 'csv export has no file_uid' do
         setup do
-          described_class.new.perform csv_export.id
+          csv_export.file_uid = nil
         end
-        it 'sets csv export file_uid' do
-          expect(csv_export.reload.file_uid).not_to be_nil
+        context 'a worker starts' do
+          setup do
+            described_class.new.perform csv_export.id
+          end
+          it 'sets csv export file_uid' do
+            expect(csv_export.reload.file_uid).not_to be_nil
+          end
         end
       end
-    end
-    context 'csv export has no file_name' do
-      setup do
-        csv_export.file_name = nil
-      end
-      context 'a worker starts' do
+      context 'csv export has no file_name' do
         setup do
-          described_class.new.perform csv_export.id
+          csv_export.file_name = nil
         end
-        it 'sets csv export file_name' do
-          expect(csv_export.reload.file_name).not_to be_nil
+        context 'a worker starts' do
+          setup do
+            described_class.new.perform csv_export.id
+          end
+          it 'sets csv export file_name' do
+            expect(csv_export.reload.file_name).not_to be_nil
+          end
         end
       end
     end
