f7c64acd705df031c2eef92abbad9bb2de55363f
Nicolas Gonzalez <nicolasgonzalez180@gmail.com>
2022-07-05 19:57:05 +0200

    Add annotations

--- spec/factories/csv_export.rb
+++ spec/factories/csv_export.rb
@@ -1,3 +1,19 @@
+# == Schema Information
+#
+# Table name: csv_exports
+#
+#  id          :bigint           not null, primary key
+#  file_name   :string
+#  file_uid    :string
+#  format_name :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  user_id     :bigint
+#
+# Indexes
+#
+#  index_csv_exports_on_user_id  (user_id)
+#
 FactoryBot.define do
   factory :csv_export do
     user
